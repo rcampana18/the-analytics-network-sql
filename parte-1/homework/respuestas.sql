f3b0d007a46fb39743621bef0e7a581a499aa84c
rcampana18 <107313647+rcampana18@users.noreply.github.com>
2023-04-03 23:15:13 -0300

    Update respuestas.sql

--- parte-1/homework/respuestas.sql
+++ parte-1/homework/respuestas.sql
@@ -184,3 +184,162 @@ SELECT orden, trunc(sum(impuestos)/sum(venta),2) as tasa
 FROM stg.order_line_sale
 GROUP BY orden
 ORDER BY orden
+
+CLASE 3
+
+1)Mostrar nombre y codigo de producto, categoria y color para todos los productos de la marca Philips y Samsung, 
+mostrando la leyenda "Unknown" cuando no hay un color disponible
+
+SELECT nombre, codigo_producto, categoria, coalesce(color,'Unknown')as Color_nulos
+FROM stg.product_master
+WHERE lower(nombre) LIKE '%philips%' or lower (nombre) LIKE '%samsung%' 
+
+2)Calcular las ventas brutas y los impuestos pagados por pais y provincia en la moneda correspondiente.
+
+SELECT sm.pais, sm.provincia, sum(ols.venta) as gross_sale, sum(ols.impuestos) as tax 
+FROM stg.order_line_sale as ols
+JOIN stg.store_master as sm
+ON ols.tienda=sm.codigo_tienda
+GROUP BY sm.pais, sm.provincia
+
+3)Calcular las ventas totales por subcategoria de producto para cada moneda ordenados por subcategoria
+y moneda.
+
+SELECT pm.subcategoria,ols.moneda,sum(ols.venta) as gross_sale
+FROM stg.order_line_sale as ols
+LEFT JOIN stg.product_master as pm
+ON ols.producto=pm.codigo_producto
+GROUP BY ols.moneda,pm.subcategoria 
+ORDER BY subcategoria, moneda
+
+4)Calcular las unidades vendidas por subcategoria de producto y la concatenacion de pais, provincia;
+usar guion como separador y usarla para ordernar el resultado.
+
+SELECT pm.subcategoria,(sm.pais||'-'||sm.provincia) as pais_pcia, sum(ols.cantidad) as unid_vendidas
+FROM stg.order_line_sale as ols
+LEFT JOIN stg.product_master as pm
+ON ols.producto=pm.codigo_producto
+LEFT JOIN stg.store_master as sm
+ON ols.tienda=sm.codigo_tienda
+GROUP BY pm.subcategoria, pais_pcia 
+ORDER BY pais_pcia
+
+5)Mostrar una vista donde sea vea el nombre de tienda y la cantidad de entradas de personas que hubo
+desde la fecha de apertura para el sistema "super_store".
+SELECT sm.nombre, sum(conteo)
+FROM stg.super_store_count as ssc
+LEFT JOIN stg.store_master as sm
+ON ssc.tienda=sm.codigo_tienda
+WHERE date(ssc.fecha::text)>sm.fecha_apertura /*por las dudas para que sea mayor a la fecha de apertura*/
+GROUP BY sm.nombre
+
+6)Cual es el nivel de inventario promedio en cada mes a nivel de codigo de producto y tienda; 
+mostrar el resultado con el nombre de la tienda.
+
+SELECT extract(month from inv.fecha) as mes,sm.nombre, inv.sku, trunc(avg((inicial+final)/2),2)as inv_prom
+FROM stg.inventory as inv
+JOIN stg.store_master as sm
+ON inv.tienda=sm.codigo_tienda
+GROUP BY mes, sm.nombre, inv.sku
+ORDER BY sm.nombre
+
+/* Inventario promedio mensual*/
+SELECT extract(month from inv.fecha) as mes, inv.sku, trunc(avg(inicial +final),2)as inv_prom
+FROM stg.inventory as inv
+GROUP BY mes, inv.sku
+
+7)Calcular la cantidad de unidades vendidas por material. 
+Para los productos que no tengan material usar 'Unknown', homogeneizar los textos si es necesario.
+
+SELECT 
+	CASE
+		WHEN pm.material is null then 'Unknown'
+		else upper(pm.material)
+		END as material_2,
+	sum(ols.cantidad)
+FROM stg.order_line_sale as ols
+LEFT JOIN stg.product_master as pm
+ON ols.producto=pm.codigo_producto
+GROUP BY pm.material 
+/*Al agrupar por material, no estoy agrupando por lo que definí en el Case, por eso el resultado
+tiene dos líneas de "plastico", porque nunca estoy mostrando la nueva columna. Esto sale con un nuevo
+Select que me traiga esa info*/
+
+With material_1 as /*material_1 sería una nueva tabla*/
+SELECT *,
+	CASE
+		WHEN material is null then 'Unknown'
+		--WHEN material = 'plastico' then 'Plastico'
+		--WHEN material = 'PLASTICO' then 'Plastico'
+		--WHEN material = 'Metal' then 'Metal'
+		else upper(material)
+		END as material_consolidado /*Sería la nueva columna*/	
+FROM stg.order_line_sale as ols
+LEFT JOIN stg.product_master as pm
+ON ols.producto=pm.codigo_producto
+)
+SELECT material_consolidado, sum(cantidad)
+FROM material_1
+GROUP BY material_consolidado
+
+8)Mostrar la tabla order_line_sales agregando una columna que represente el valor de venta bruta 
+en cada linea convertido a dolares usando la tabla de tipo de cambio.
+
+/*Quiero Joinear con el mes las dos tablas y las ventas brutas dividirlas por la cotizacion para 
+tener el valor en usd.
+--OLS tiene fechas del 2022
+--mafr tiene los meses del 2022 y 2023 */
+
+SELECT *,  /*Cuándo es necesario referenciar la tabla de origen en el case?? Donde está la columna "moneda" por ejemplo*/
+	CASE
+		WHEN moneda='ARS' then ols.venta/mafr.cotizacion_usd_peso
+		WHEN moneda='EUR' then ols.venta/mafr.cotizacion_usd_eur
+		WHEN moneda='URU' then ols.venta/mafr.cotizacion_usd_uru
+	END AS venta_bruta_usd,
+	(TO_CHAR (fecha, 'YYYY-MM')) AS fecha,
+    (TO_CHAR (mes, 'YYYY-MM')) AS mes
+FROM stg.order_line_sale as ols
+LEFT JOIN stg.monthly_average_fx_rate as mafr
+ON extract(month from ols.fecha)=extract(month from mafr.mes)
+	
+9)Calcular cantidad de ventas totales de la empresa en dolares.
+
+With tabla_nueva as(
+SELECT *,  
+	CASE
+		WHEN moneda='ARS' then (ols.venta+coalesce(ols.descuento,0)+coalesce(ols.creditos,0))/mafr.cotizacion_usd_peso
+		WHEN moneda='EUR' then (ols.venta+coalesce(ols.descuento,0)+coalesce(ols.creditos,0))/mafr.cotizacion_usd_eur
+		WHEN moneda='URU' then (ols.venta+coalesce(ols.descuento,0)+coalesce(ols.creditos,0))/mafr.cotizacion_usd_uru
+	END AS venta_neta_usd
+FROM stg.order_line_sale as ols
+LEFT JOIN stg.monthly_average_fx_rate as mafr
+ON extract(month from ols.fecha)=extract(month from mafr.mes)
+)
+SELECT trunc(sum(venta_neta_usd),2) as Tot_Venta_USD
+FROM tabla_nueva
+
+10)Mostrar en la tabla de ventas el margen de venta por cada linea. Siendo margen = (venta - promociones) - costo expresado en dolares.
+
+With nueva_tabla as(
+SELECT *,  
+	CASE
+		WHEN moneda='ARS' then (ols.venta+coalesce(ols.descuento,0)+coalesce(ols.creditos,0))/mafr.cotizacion_usd_peso
+		WHEN moneda='EUR' then (ols.venta+coalesce(ols.descuento,0)+coalesce(ols.creditos,0))/mafr.cotizacion_usd_eur
+		WHEN moneda='URU' then (ols.venta+coalesce(ols.descuento,0)+coalesce(ols.creditos,0))/mafr.cotizacion_usd_uru
+	END AS venta_neta_usd
+FROM stg.order_line_sale as ols
+LEFT JOIN stg.monthly_average_fx_rate as mafr
+ON extract(month from ols.fecha)=extract(month from mafr.mes)
+LEFT JOIN stg.cost as cst
+ON ols.producto=cst.codigo_producto
+)
+SELECT *, venta_neta_usd-costo_promedio_usd as margen
+FROM nueva_tabla
+	
+11)Calcular la cantidad de items distintos de cada subsubcategoria que se llevan por numero de orden.
+
+SELECT orden, subcategoria, count(producto)
+FROM stg.order_line_sale as ols
+LEFT JOIN stg.product_master as pm
+ON ols.producto=pm.codigo_producto
+GROUP BY orden, subcategoria
